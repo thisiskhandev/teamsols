42b1f7e8deed1d66788c9e231b1c8da17b71a267
Hassan Khan <hassankhan427726@gmail.com>
2021-11-05 04:25:41 +0500

    Create career form

--- src/Pages/Components/Careers.js
+++ src/Pages/Components/Careers.js
@@ -22,47 +22,81 @@ const Careers = () => {
                   </p>
                 </div>
                 <div className="col-lg-6 col-md-6 col-sm-12">
-                  <form>
-                    <h2>Share your details</h2>
-                    <small>We will get back to you ASAP.</small>
+                  <form className="forms">
+                    <article>
+                      <h3>Share your details</h3>
+                      <em>We will get back to you ASAP.</em>
+                    </article>
                     <div className="mb-3">
-                      <label htmlFor="name" className="form-label">
-                        Name
-                      </label>
                       <input
                         type="text"
                         className="form-control"
                         id="name"
                         aria-describedby="text"
-                        placeholder="name"
+                        placeholder="Name"
                       />
                     </div>
                     <div className="mb-3">
-                      <label
-                        htmlFor="exampleInputEmail1"
-                        className="form-label"
-                      >
-                        Email address
-                      </label>
                       <input
                         type="email"
                         className="form-control"
                         id="exampleInputEmail1"
                         aria-describedby="emailHelp"
+                        placeholder="your@mail.com"
                       />
                     </div>
                     <div className="mb-3">
-                      <label
-                        htmlFor="exampleInputPassword1"
-                        className="form-label"
-                      >
-                        Password
-                      </label>
                       <input
-                        type="password"
+                        type="tel"
                         className="form-control"
-                        id="exampleInputPassword1"
+                        id="phoneNum"
+                        placeholder="Phone"
                       />
+                    </div>
+                    <div className="mb-3">
+                      <select
+                        type=""
+                        className="form-control"
+                        id="serviceType"
+                        required
+                      >
+                        <option selected disabled hidden>
+                          Select the service you want for your project
+                        </option>
+                        <option value="Customized Solution Development">
+                          Customized Solution Development
+                        </option>
+                        <option value="Digital Transformation">
+                          Digital Transformation
+                        </option>
+                        <option value="Technical Human resourcing">
+                          Technical Human resourcing
+                        </option>
+                        <option value="Oracle Implementation">
+                          Oracle Implementation
+                        </option>
+                        <option value="Web Development">Web Development</option>
+                        <option value="Mobile Apps Development">
+                          Mobile Apps Development
+                        </option>
+                        <option value="E-commerce Development">
+                          E-commerce Development
+                        </option>
+                        <option value="Creative UI/UX designing">
+                          Creative UI/UX designing
+                        </option>
+                        <option value="Digital Marketing">
+                          Digital Marketing
+                        </option>
+                      </select>
+                      <textarea
+                        name=""
+                        id=""
+                        cols="30"
+                        rows="10"
+                        className="form-control"
+                        placeholder="Tell us about your project (Max 300 characters)"
+                      ></textarea>
                     </div>
                     <button type="submit" className="btn btn-primary">
                       Apply Now
